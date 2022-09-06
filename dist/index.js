1bd21f8acfef85907a8a99fd8aa1a572a0ec6682
Morten Duus <mdu@planday.com>
2022-09-06 14:15:53 +0200

    testing

--- dist/index.js
+++ dist/index.js
@@ -4,24 +4,27 @@ const repoName = core.getInput('repo-name');
 const orgName = core.getInput('org-name');
 const ghToken = core.getInput('org-admin-token');
 console.log(`Creating repository ${repoName} in organization ${orgName}`);
-try {
-    var result = await request("GET /orgs/{org}/repos", {
-        headers: {
-            authorization: `token ${ghToken}`,
-        },
-        org: orgName,
-        name: repoName,
-        description: 'This is a demo repo created from an action repository',
-        homepage: 'https://github.com',
-        'private': true,
-        has_issues: true,
-        has_projects: true,
-        has_wiki: true
-    });
-    core.setOutput("repo-fullname", result.full_name);
-    core.setOutput("repo-url", result.url);
-}
-catch (e) {
-    core.setFailed(e.message);
-}
+(async function main() {
+    try {
+        var result = await request("GET /orgs/{org}/repos", {
+            headers: {
+                authorization: `token ${ghToken}`,
+            },
+            org: orgName,
+            name: repoName,
+            description: 'This is a demo repo created from an action repository',
+            homepage: 'https://github.com',
+            'private': true,
+            has_issues: true,
+            has_projects: true,
+            has_wiki: true
+        });
+        core.setOutput("repo-fullname", result.full_name);
+        core.setOutput("repo-url", result.url);
+    }
+    catch (e) {
+        core.setFailed(e.message);
+    }
+    // You can use await inside this function block
+})();
 export {};
