cd8fd16b2088378204446ad9a486ca9cd270b98b
Morten Duus <mdu@planday.com>
2022-09-06 11:54:11 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,27 @@ const repoName = core.getInput('repo-name');
 const orgName = core.getInput('org-name');
 const ghToken = core.getInput('org-admin-token');
 
-try {
-    var result = await request("GET /orgs/{org}/repos", {
-        headers: {
-          authorization: `token ${ ghToken }`,
-        },
-        org: orgName,
-        name: repoName,
-        description: 'This is a demo repo created from an action repository',
-        homepage: 'https://github.com',
-        'private': true,
-        has_issues: true,
-        has_projects: true,
-        has_wiki: true
-    })
+const main = async () => 
+{
+  try {
+      var result = await request("GET /orgs/{org}/repos", {
+          headers: {
+            authorization: `token ${ ghToken }`,
+          },
+          org: orgName,
+          name: repoName,
+          description: 'This is a demo repo created from an action repository',
+          homepage: 'https://github.com',
+          'private': true,
+          has_issues: true,
+          has_projects: true,
+          has_wiki: true
+      })
 
-    core.setOutput("repo-fullname", result.full_name);
-    core.setOutput("repo-url", result.url);
+      core.setOutput("repo-fullname", result.full_name);
+      core.setOutput("repo-url", result.url);
 
-} catch (e: any) {
-    core.setFailed(e.message);
-  }
-
-export { };
+  } catch (e: any) {
+      core.setFailed(e.message);
+    }
+}
